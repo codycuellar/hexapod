3f92fc0f2af5c000f3a065b6d6d98fbf19e74f1d
Cody Cuellar <cody.cuellar@gmail.com>
2025-03-20 20:20:36 -0500

    Move python libs to src/ update main.

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ from hexapod.leg import Leg, ServoConfig
 from pimoroni import Button
 from servo import Servo, servo2040
 
-def lerp(v1:float, v2:float, t:float):
-    return v1 + (v2 - v1) * t
-
 USER_BUTTON = Button(servo2040.USER_SW)
 
 UPDATES_PER_SEC = 50
@@ -15,14 +12,37 @@ WALK_CYCLE_TIME = 2000
 
 
 l1_coxa = ServoConfig(servo2040.SERVO_1, 80, -80, 0)
-l1_femur = ServoConfig(servo2040.SERVO_2, -47, 90, 27.65, True)
-l1_tibia = ServoConfig(servo2040.SERVO_3, -70, 90, 0, True)
-leg = Leg(l1_coxa, l1_femur, l1_tibia, femur_len=65, tibia_len=90)
+l1_femur = ServoConfig(servo2040.SERVO_2, -47, 90, 45, True)
+l1_tibia = ServoConfig(servo2040.SERVO_3, 70, -70, 80, False)
+leg = Leg(l1_coxa, l1_femur, l1_tibia, coxa_len=40, femur_len=65, tibia_len=90)
 start_time = ticks_ms()
 
-p1 = [90, 50, -100]
-p2 = [30, -50, -100]
-p3 = [(90+30)/2, 0, 0]
+
+walk_cycle_points = [
+    [100, 50, -40],
+    [100, -50, -40],
+    [100, 0, 0]
+]
+def walk_cycle(t):
+    if (t < .5):
+        t = t * 2
+        pos_x = interpolation.lerp(walk_cycle_points[0][0], walk_cycle_points[1][0], t)
+        pos_y = interpolation.lerp(walk_cycle_points[0][1], walk_cycle_points[1][1], t)
+        pos_z = interpolation.lerp(walk_cycle_points[0][2], walk_cycle_points[1][2], t)
+    else:
+        t = (t - .5) * 2
+        pos_x = interpolation.quad_bez(walk_cycle_points[1][0], walk_cycle_points[2][0], walk_cycle_points[0][0], t)
+        pos_y = interpolation.quad_bez(walk_cycle_points[1][1], walk_cycle_points[2][1], walk_cycle_points[0][1], t)
+        pos_z = interpolation.quad_bez(walk_cycle_points[1][2], walk_cycle_points[2][2], walk_cycle_points[0][2], t)
+    
+    return pos_x, pos_y, pos_z
+
+circle_center_point = [100, 0, -40]
+def circle_pattern(t):
+    x = circle_center_point[0] + 50 * math.cos(t)
+    y = circle_center_point[1] + 50 * math.sin(t)
+    z = circle_center_point[2]
+    return x, y, z
 
 sleep(1)
 while not USER_BUTTON.raw():
@@ -30,20 +50,10 @@ while not USER_BUTTON.raw():
     elapsed = (ticks - start_time) % WALK_CYCLE_TIME
     cycle_t = elapsed / WALK_CYCLE_TIME  # Normalize time to 0 → 1
 
-    if (cycle_t < .5):
-        t = cycle_t * 2
-        pos_x = lerp(p1[0], p2[0], t)
-        pos_y = lerp(p1[1], p2[1], t)
-        pos_z = lerp(p1[2], p2[2], t)
-    else:
-        t = (cycle_t - .5) * 2
-        pos_x = interpolation.quad_bez(p2[0], p3[0], p1[0], t)
-        pos_y = interpolation.quad_bez(p2[1], p3[1], p1[1], t)
-        pos_z = interpolation.quad_bez(p2[2], p3[2], p1[2], t)
-
-    angles = leg.set_position(pos_x, pos_y, pos_z)
+    # walk_cycle(cycle_t)
+    point = circle_pattern(cycle_t * 2 * math.pi)
+    leg.set_position(*point)
 
     sleep(1 / UPDATES_PER_SEC)
 
 leg.zero_servos()
-
--- src/hexapod/leg.py
+++ src/hexapod/leg.py
@@ -27,7 +27,7 @@ class Leg:
     """ (upward, downward) absolute angle limits for the tibia joint. """
     tibia_limits = (-70, 90)
 
-    def __init__(self, coxa: ServoConfig, femur: ServoConfig, tibia: ServoConfig, femur_len=50, tibia_len=50):
+    def __init__(self, coxa: ServoConfig, femur: ServoConfig, tibia: ServoConfig, coxa_len=50, femur_len=50, tibia_len=50):
         """
         Initialize the leg with servo pin numbers for each joint (coxa, femur, tibia). Offsets
         specify the angle of the coordinate plane relative to the body and ground plane when 
@@ -44,6 +44,7 @@ class Leg:
         self.femur_config = femur
         self.tibia_config = tibia
 
+        self.coxa_len = coxa_len
         self.femur_len = femur_len
         self.tib_len = tibia_len
 
@@ -67,9 +68,9 @@ class Leg:
         Z is up/down, X is in/out from body, y is forward/backwards.
         """
         a1 = math.degrees(math.atan2(y, x))
-        xyL = math.sqrt(y**2 + x**2)
-        zH = math.sqrt(z**2 + xyL**2)
-        z_theta = math.atan2(z, xyL)
+        xyH = max(0, math.sqrt(y**2 + x**2) - self.coxa_len)
+        zH = math.sqrt(z**2 + xyH**2)
+        z_theta = math.atan2(z, xyH)
         a2cos = (self.femur_len**2 + zH**2 - self.tib_len**2) / (2 * self.femur_len * zH)
         a2 = math.degrees(math.acos(a2cos) + z_theta)
 
@@ -81,21 +82,19 @@ class Leg:
     def _normalize_relative_angles(self, a1, a2, a3):
         coxa_angle = a1 - self.coxa_config.zeroed_angle if not self.coxa_config.inverted else self.coxa_config.zeroed_angle - a1
         coxa_angle = self._clamp(coxa_angle, min(self.coxa_limits), max(self.coxa_limits))
-        print('a2', a2, 'femur_zero', self.femur_config.zeroed_angle, 'inverted', self.femur_config.inverted)
         femur_angle = a2 - self.femur_config.zeroed_angle if not self.femur_config.inverted else self.femur_config.zeroed_angle - a2
         femur_angle = self._clamp(femur_angle, min(self.femur_limits), max(self.femur_limits))
 
-        tibia_angle = 90 - a3 - self.tibia_config.zeroed_angle if not self.tibia_config.inverted else self.tibia_config.zeroed_angle - (90 - a3)
+        tibia_angle = a3 - self.tibia_config.zeroed_angle if not self.tibia_config.inverted else self.tibia_config.zeroed_angle - a3
         tibia_angle = self._clamp(tibia_angle, min(self.tibia_limits), max(self.tibia_limits))
 
-        print('normalized angles:', coxa_angle, femur_angle, tibia_angle)
         return (coxa_angle, femur_angle, tibia_angle)
 
     def _clamp(self, value, min_val, max_val):
         return max(min(value, max_val), min_val)
     
     def _set_servo_angles(self, s1, s2, s3):
-        # self.scoxa.value(s1)
-        # self.sfemur.value(s2)
-        # self.stibia.value(s3)
+        self.scoxa.value(s1)
+        self.sfemur.value(s2)
+        self.stibia.value(s3)
         return (s1, s2, s3)
